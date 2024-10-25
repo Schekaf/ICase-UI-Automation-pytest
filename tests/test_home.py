bd40c9b99b497d3a088cb61f5a9c99ef561fa3fc
Furkan Kızıltepe <furkan.kiziltepe@gmail.com>
2024-10-25 19:05:47 +0300

    test update

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,14 +1,14 @@
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 from pages.home_page import HomePage
 
 
 def test_open_home_page(driver):
     home_page = HomePage(driver)
-
+    # Check Home Page Title
     actual_title = driver.title
     assert actual_title == home_page.title, f"Expected title to be '{home_page.title}' but got '{actual_title}'"
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(home_page.logo))
+    # Check Home Page Logo
+    home_page.wait_logo_to_be_loaded()
     logo_element = driver.find_element(*home_page.logo)
     assert logo_element.is_displayed(), f"Home page couldn't be loaded!"

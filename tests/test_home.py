51303a7bf666cdac33766fc8f954337e490d6971
Furkan Kızıltepe <furkan.kiziltepe@gmail.com>
2024-10-25 20:46:51 +0300

    update wait home page loading

--- tests/test_home.py
+++ tests/test_home.py
@@ -9,6 +9,6 @@ def test_open_home_page(driver):
     actual_title = driver.title
     assert actual_title == home_page.title, f"Expected title to be '{home_page.title}' but got '{actual_title}'"
     # Check Home Page Logo
-    home_page.wait_logo_to_be_loaded()
+    home_page.wait_home_page_to_be_loaded()
     logo_element = driver.find_element(*home_page.logo)
     assert logo_element.is_displayed(), f"Home page couldn't be loaded!"

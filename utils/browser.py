b6fa0616978eba04e41af76cc91c4fc57a7a8d47
Furkan Kızıltepe <furkan.kiziltepe@gmail.com>
2024-10-26 02:33:22 +0300

    browser options extended

--- utils/browser.py
+++ utils/browser.py
@@ -12,9 +12,15 @@ def pytest_addoption(parser):
 def get_driver(browser_name):
     if browser_name == "chrome":
         options = ChromeOptions()
+        prefs = {
+            "profile.default_content_setting_values.cookies": 2,  # 2 = Block all cookies
+            "profile.block_third_party_cookies": True  # Block third-party cookies
+        }
+        options.add_experimental_option("prefs", prefs)
         driver = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         options = FirefoxOptions()
+        options.set_preference("network.cookie.cookieBehavior", 2)
         driver = webdriver.Firefox(options=options)
     else:
         raise ValueError(f"Unsupported browser: {browser_name}")

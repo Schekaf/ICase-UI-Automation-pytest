300a6af4e448887d9968c089c28dcac792dbd77d
Furkan Kızıltepe <furkan.kiziltepe@gmail.com>
2024-10-25 23:41:08 +0300

    Teams test done

--- tests/test_teams.py
+++ tests/test_teams.py
@@ -13,7 +13,10 @@ def test_teams(driver):
     careers_page.wait_careers_page_to_be_loaded()
     careers_page.scroll(careers_page.careers_locations_move_right)
     careers_page.click_see_all_jobs()
-    actual_teams_list = careers_page.get_teams()
+    actual_teams_list = careers_page.get_teams_title_list()
     expected_teams_list = careers_page.jobs_list
     assert all(item in actual_teams_list for item in expected_teams_list), (
-            f"Missing Locations found : " + str(list(set(expected_teams_list) - set(actual_teams_list))))
+            f"Missing Teams found : " + str(list(set(expected_teams_list) - set(actual_teams_list))))
+    assert careers_page.is_team_blocks_open()
+
+

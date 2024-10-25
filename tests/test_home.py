49283e5d452234c441e9d5ebad981c1413733f40
Furkan Kızıltepe <furkan.kiziltepe@gmail.com>
2024-10-25 18:55:42 +0300

    Test 1 Completed

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,6 +1,14 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-def test_open_home_page(setup):
-    driver = setup
-    expected_title = "#1 Leader in Individualized, Cross-Channel CX — Insider"
+from pages.home_page import HomePage
+
+
+def test_open_home_page(driver):
+    home_page = HomePage(driver)
+
     actual_title = driver.title
-    assert actual_title == expected_title, f"Expected title to be '{expected_title}' but got '{actual_title}'"
+    assert actual_title == home_page.title, f"Expected title to be '{home_page.title}' but got '{actual_title}'"
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(home_page.logo))
+    logo_element = driver.find_element(*home_page.logo)
+    assert logo_element.is_displayed(), f"Home page couldn't be loaded!"

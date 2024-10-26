d1677c2a85c270b1bf2bfc7b58df268a9f400a2a
Furkan Kızıltepe <furkan.kiziltepe@gmail.com>
2024-10-26 03:23:45 +0300

    take screenshots and save them / gitignore screenshots folder

--- conftest.py
+++ conftest.py
@@ -1,3 +1,6 @@
+import datetime
+import os
+
 import pytest
 from utils.browser import get_driver
 from utils.common import Common
@@ -17,4 +20,24 @@ def driver(request):
     yield driver
     driver.quit()
 
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # Run the test
+    outcome = yield
+    report = outcome.get_result()
+
+    # Check if the test failed
+    if report.when == "call" and report.failed:
+        # Take a screenshot if the test failed
+        screenshot_dir = 'screenshots'
+        os.makedirs(screenshot_dir, exist_ok=True)  # Create directory if it doesn't exist
+
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        screenshot_path = os.path.join(screenshot_dir, f"{item.name}_{timestamp}.png")
+
+        # Get the driver from the current test context
+        driver = item.funcargs['driver']
+        driver.save_screenshot(screenshot_path)
+        print(f"Screenshot saved at: {screenshot_path}")
 
